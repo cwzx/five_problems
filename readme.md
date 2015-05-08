10bbbf4b2f53e53d23c40ea7d2c4456691bcc45f
cwzx <chriswelshman@gmail.com>
2015-05-08 19:51:47 +0100

    initial

---
+++ readme.md
@@ -0,0 +1,30 @@
+# Five Problems
+
+Problems found [here](https://blog.svpino.com/2015/05/07/five-programming-problems-every-software-engineer-should-be-able-to-solve-in-less-than-1-hour).
+
+Solutions in C++.
+
+## Problem 5
+
+* Brute force
+* Single-threaded
+* No dynamic allocation
+* 85 lines of code
+* Run time: ~8.7 ms
+
+Output:
+
+```cpp
+123 - 45 - 67 + 89
+12 - 3 - 4 + 5 - 6 + 7 + 89
+12 + 3 + 4 + 5 - 6 - 7 + 89
+123 + 4 - 5 + 67 - 89
+1 + 2 + 3 - 4 + 5 + 6 + 78 + 9
+12 + 3 - 4 + 5 + 67 + 8 + 9
+1 + 23 - 4 + 56 + 7 + 8 + 9
+1 + 2 + 34 - 5 + 67 - 8 + 9
+1 + 23 - 4 + 5 + 6 + 78 - 9
+123 + 45 - 67 + 8 - 9
+123 - 4 - 5 - 6 - 7 + 8 - 9
+```
+
---
+++ src/problem5.cpp
@@ -0,0 +1,85 @@
+#include <array>
+#include <cstdio>
+
+enum Symbol {
+	Nothing,
+	Plus,
+	Minus
+};
+
+using Symbols = std::array<Symbol,8>;
+
+int evaluate( Symbols sym ) {
+	
+	std::array<int,9> values = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	// combine nothings
+	for(int i=0;i<8;) {
+		if( sym[i] == Nothing ) {
+			values[i] = values[i] * 10 + values[i+1];
+			
+			// shift all the rest down
+			for(int j=i+1;j<8;++j)
+				values[j] = values[j+1];
+			values[8] = 0;
+			
+			for(int j=i;j<7;++j)
+				sym[j] = sym[j+1];
+			sym[7] = Plus;
+		} else {
+			++i;
+		}
+	}
+
+	// convert subtraction to addition
+	for(int i=0;i<8;++i) {
+		if( sym[i] == Minus ) {
+			values[i+1] = -values[i+1];
+			sym[i] = Plus;
+		}
+	}
+
+	int sum = 0;
+	for( auto x : values ) 
+		sum += x;
+	return sum;
+}
+
+bool next_combination( Symbols& sym ) {
+	for(int i=0;i<8;++i) {
+		if( sym[i] != Minus ) {
+			sym[i] = Symbol((int)sym[i] + 1);
+			break;
+		} else {
+			sym[i] = Nothing;
+			if( i == 7 )
+				return false;
+		}
+	}
+	return true;
+}
+
+void print( const Symbols& sym ) {
+	for(int i=0;i<8;++i) {
+		printf("%d",i+1);
+		switch( sym[i] ) {
+			case Plus:
+			printf(" + ");
+			break;
+			case Minus:
+			printf(" - ");
+			break;
+		}
+	}
+	printf("9\n");
+}
+
+int main() {
+	Symbols sym = {};
+	
+	do {
+		if( evaluate( sym ) == 100 ) {
+			print( sym );
+		}
+	} while( next_combination( sym ) );
+}
